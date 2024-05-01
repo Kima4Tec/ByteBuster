02285257ab66a994a5ee818940a8f8063adf34af
Kima4Tec <150744968+Kima4Tec@users.noreply.github.com>
2024-05-01 10:25:37 +0200

    Add project files.

---
+++ Grafik.cs
@@ -0,0 +1,164 @@
+﻿using ByteBuster;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ByteBuster //Programmet er skrevet af Kim Massesson
+{
+    internal class Grafik
+    {
+        public void Skriv(int x, int y, string tekst) //en metode, hvor jeg kan positionere en tekst
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(tekst);
+        }
+        public void ByteBusterLogo(int x, int y) //jeg har lavet et logo tegnet med x'er, som jeg kan positionere med Skriv-metoden
+        {
+            Skriv(x, y + 0, "                                                                                              ");
+            Skriv(x, y + 1, "  xxxxxxxxxxx  xxx        xxx  xxxxxxxxxxx xxxxxxxxxxx                                        ");
+            Skriv(x, y + 2, "  xxx   xxxxxx  xxx      xxx   xxxxxxxxxxx xxxxxxxxxxx                                        ");
+            Skriv(x, y + 3, "  xxx     xxxxx  xxx    xxx        xxx     xxx                                                ");
+            Skriv(x, y + 4, "  xxx    xxxx     xxx  xxx         xxx     xxx                                                ");
+            Skriv(x, y + 5, "  xxxxxxxxxxxx     xxxxx           xxx     xxxxxxxxxxx                                        ");
+            Skriv(x, y + 6, "  xxxxxxxxxxxx      xxx            xxx     xxxxxxxxxxx  xxx                                   ");
+            Skriv(x, y + 7, "  xxx      xxxx     xxx            xxx     xxx          x  x  x  x xxxx xxxxx xxxx xx         ");
+            Skriv(x, y + 8, "  xxx      xxxxx    xxx            xxx     xxx          xxx   x  x x      x   x    x x        ");
+            Skriv(x, y + 9, "  xxx   xxxxxxx     xxx            xxx     xxx          x  x  x  x xxxx   x   xxx  xx         ");
+            Skriv(x, y + 10, "  xxxxxxxxxxxxx     xxx            xxx     xxxxxxxxxxx  x   x x  x    x   x   x    x x        ");
+            Skriv(x, y + 11, "  xxxxxxxxxxxx      xxx            xxx     xxxxxxxxxxx  xxxx  xxxx xxxx   x   xxxx x  x ©CMIS ");
+            Skriv(x, y + 12, "                                                                                              ");
+        }
+        public void EmptyBox(int x, int y) //denne kan jeg bruge som en clear af det tekst-område som jeg bruger
+        {
+            Skriv(x, y + 0, "                                                                                             ");
+            Skriv(x, y + 1, "                                                                                             ");
+            Skriv(x, y + 2, "                                                                                             ");
+            Skriv(x, y + 3, "                                                                                             ");
+            Skriv(x, y + 4, "                                                                                             ");
+            Skriv(x, y + 5, "                                                                                             ");
+            Skriv(x, y + 6, "                                                                                             ");
+            Skriv(x, y + 7, "                                                                                             ");
+            Skriv(x, y + 8, "                                                                                             ");
+            Skriv(x, y + 9, "                                                                                             ");
+        }
+        /*introtekst med time-delay. Jeg brugte meget tid på at finde ud af, 
+         *hvordan jeg kunne lave et daly i tid og fandt på at lave et loop med DateTime,
+         *inden jeg fandt ud af, at man kan bruge Thread.Sleep() til at pause prog. Derfor anvender jeg først loopet, 
+         *for at vise det, og dernæst bruger jeg Thread.Sleep()*/
+        public void IntroTekst(int x, int y) 
+        {
+            Skriv(x + 5, y + 0, "Christian Mørk er altid på talentjagt");
+
+            int tid = 2;
+            //Første tidsforløb
+            DateTime a = DateTime.Now;
+            DateTime b = a.AddSeconds(tid);
+            for (int i = 0; i < 100000000; i++) // for-løkken skal hele tiden opdatere sekundNu og gå ud af løkken, når der er gået {tid} sekunder
+            {
+                DateTime c = DateTime.Now;     //prog henter tiden lige nu og smider ind i variablen c1
+                int sekundNu1 = c.Second;      //jeg skal kun bruge sekunder og smider dem i integer variablen sekundNu1
+                int nytSekund1 = b.Second;     //nytSekund1 danner jeg ved at lægge to sekunder til vha: DateTime b1 = a1.AddSeconds(tid)
+
+                if (sekundNu1 != nytSekund1)    //når sekundNu1 == nytSekund1 er der gået {tid} sekunder
+                {
+                    Console.SetCursorPosition(0, 0);//jeg flytter cursoren op i venstre top hjørne så den ikke tager opmærksomhed fra teksten
+                }
+                else
+                {
+                    i = 100000000;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    EmptyBox(13, 14);
+                }
+            }
+
+            Skriv(x - 1, y + 0, "Christian Mørk ønsker dig held og lykke med spillet.");
+                Console.SetCursorPosition(0, 0);
+                Thread.Sleep(2000);
+                Console.BackgroundColor = ConsoleColor.Black;
+                EmptyBox(13, 14);
+
+            Skriv(x + 7, y + 0, "Vinder du, får du en lille præmie.");
+                Console.SetCursorPosition(0, 0);
+                Thread.Sleep(2000);
+                Console.BackgroundColor = ConsoleColor.Black;
+                EmptyBox(13, 14);
+ 
+            Skriv(x + 5, y + 0, "De bedste hilsener fra Christian Mørk.");
+                Console.SetCursorPosition(0, 0);
+                Thread.Sleep(2000);
+                Console.BackgroundColor = ConsoleColor.Black;
+                EmptyBox(13, 14);
+
+            Skriv(x + 45, y + 4, "Og så er vi i gang... ");
+                Thread.Sleep(2000);
+                Console.BackgroundColor = ConsoleColor.Black;
+                EmptyBox(13, 14);
+        }
+
+        public void Win(int x, int y) //Vinderteksten
+        {
+            Skriv(x, y + 0, "Tillykke, du har vundet!");
+            Skriv(x, y + 1, "Kontakt mig på telefon 0x24688E8"); //Bonusinfo: Hvis man omdanner nummeret bliver det: 38177000
+
+            Skriv(x, y + 3, "Venlig hilsen Christian");
+
+            Skriv(x, y + 5, "Fortsat god festival og husk nu at få din præmie!");
+        }
+
+        public void Rammer() //rammerne omkring logo og tekstfeltet
+        {
+            //Her tegner jeg et rektangel
+
+            //første vandrette streg
+            for (int i = 0; i < 93; i++)
+            {
+                Skriv(13 + i, 1, "─");
+            }
+            //anden vandrette stribe
+            for (int i = 0; i < 93; i++)
+            {
+                Skriv(13 + i, 13, "─");
+            }
+            //tredje vandrette stribe
+            for (int i = 0; i < 93; i++)
+            {
+                Skriv(13 + i, 24, "─");
+            }
+            //venstre lodret streg
+            for (int i = 0; i < 22; i++)
+            {
+                Skriv(12, 2 + i, "│");
+            }
+            //højre lodret streg
+            for (int i = 0; i < 22; i++)
+            {
+                Skriv(106, 2 + i, "│");
+            }
+            //venstre top hjørne
+            Skriv(12, 1, "┌");
+
+            //Højre top hjørne
+            Skriv(106, 1, "┐");
+
+            //Venstre ml hjørne
+            Skriv(12, 13, "├");
+
+            //Højre ml hjørne
+            Skriv(106, 13, "┤");
+
+            //Venstre ml hjørne
+            Skriv(12, 24, "└");
+
+            //Højre nedre hjørne
+            Skriv(106, 24, "┘");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,159 @@
+﻿using ByteBuster;
+using System;
+using System.Timers;    
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Schema;
+using System.Media;
+
+namespace ByteBuster
+{
+    internal class Program//Programmet er skrevet af Kim Massesson. Mit mål er at bruge det meste af, hvad jeg har lært i dette modul.
+    {
+        static void Main(string[] args)                
+        {
+            Grafik Pos = new Grafik();
+
+            int antalRigtige = 0;
+            int antalForkerte = 0;
+
+            //Grafikken sættes op bl.a. ved at hente metoder, som jeg har lavet, fra anden klasse. Men først vælger jeg tekst- og baggrundsfarve
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            
+            Pos.ByteBusterLogo(12, 1);
+            Pos.Rammer();
+            Console.ResetColor();
+
+            //her kalder jeg introteksten frem fra Grafik-klassen. Der kommer en sætning frem ad gangen ved et tids-delay. Jeg bruger både et delay, som jeg selv har lavet og Thread.Sleep
+            Pos.IntroTekst(34, 18);
+            Console.Beep(400,1000);
+            //SystemSounds.Asterisk.Play();
+            /*Jeg laver en menu med et do-while loop og med en switch, hvor man kan vælge sværhedsgrad.
+             *Variablen "maks" bruger jeg til at sætte max-værdien i RandomObj(min,max) og "valg" til 
+             *at indholde brugerens valg. Man kan vælge mellem "let" som er 32, "øvet" 128 eller "avanceret" 255.*/
+            string valg;
+            int maks=32;
+            do
+            {
+
+                Pos.Skriv(46,15,"* Vælg sværhedsgrad *");
+                Pos.Skriv(46, 17, "Let:         [Tast 1]");
+                Pos.Skriv(46, 18, "Øvet         [Tast 2]");
+                Pos.Skriv(46, 19, "Avanceret:   [Tast 3]");
+                Pos.Skriv(46, 20,$"                     ");
+                Console.SetCursorPosition(65, 20);
+                var enTast = Console.ReadKey();
+                valg = enTast.KeyChar.ToString();
+                
+            }
+            while (valg != "1" && valg != "2" && valg != "3");
+            {
+                switch (valg)
+                {
+                    case "1":
+                        {
+                            maks = 33;
+                            break;
+                        }
+                    case "2":
+                        {
+                            maks = 129;
+                            break;
+                        }
+                    case "3":
+                        {
+                            maks = 256;
+                            break;
+                        }
+
+                    default:
+                        {
+                            Pos.Skriv(32, 22, "Du skal vælge en sværhedsgrad.");
+                            break;
+                        }
+                }
+            }
+
+            //lav et tilfældigt binært tal, som bruger skal gætte
+            for (int i = 1; i < 6; i++)                                             //loop, der tæller forsøgene. Der må max være fem forsøg
+            {
+                Random RandomObj = new Random();                                        //her laver prog en ny instans så der kan findes en tilfældig byte
+                int nummer = RandomObj.Next(1, maks); 
+                Int32 svar = 0;                                                     //svar får en værdi
+                string bin = Convert.ToString(nummer, 2);
+                /*Jeg omdanner det binære tal, som har type string til integer for så
+                 * kan jeg formatere den med D8, og dermed få 0 foran det binære tal.
+                 * jeg kunne have gjort det samme med Padleft(8,'0'). Det bruger jeg i min
+                 * anden opgave*/
+                Int32 nytnr = Convert.ToInt32(bin);                           
+
+                //fang andre indtastninger end integer - tal og fortæl bruger, at denne skal skrive et tal. Man bruger et forsøg på fejlen
+                try
+                {
+                    Pos.EmptyBox(13, 14);
+                    Pos.Skriv(32, 22, $"Antal rigtige svar: {antalRigtige}");
+                    Pos.Skriv(55, 22, $"Antal forkerte svar: {antalForkerte}");
+                    Pos.Skriv(32, 15, $"Forsøg nr: {i}: ");
+                    Pos.Skriv(32, 16, "Omskriv det nedenstående binære tal til et decimaltal.");
+                    Pos.Skriv(32, 17, $"{nytnr:D8}");                                //det binære tal bliver fyldt ud med nuller pga D8 (Fra Microsoft:"The precision specifier indicates the minimum number of digits desired in the resulting string. If required, the number is padded with zeros to its left to produce the number of digits given by the precision specifier")
+                    Pos.Skriv(32, 18, "Indtast dit svar: ");                        //brugerindtastning
+                    svar = Convert.ToInt32(Console.ReadLine());                     //{svar}s type skal gerne passe til {nummer}s type og bliver derfor konverteret til Int32
+                }
+                catch
+                {
+                    Pos.Skriv(32, 18, $"Du skal skrive et tal. ");                    //udskrift hvis bruger skriver andet end integer-tal
+                }
+
+            if (svar == nummer)                                                 //sandt eller falsk
+                {
+                    antalRigtige++;                                                 //hvis sandt lægges der en værdi til i variablen antalRigtige
+                    
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Pos.Skriv(32, 19,"Det var rigtigt! Tryk [ENTER] for at gå videre");
+                    Console.ResetColor();
+                    
+                    if (antalRigtige >= 3)                                          //undersøg om bruger har haft tre vellykkede forsøg
+                    {
+                        Pos.EmptyBox(13, 14);
+                        Pos.Win(34, 16);
+                        i = 6;
+                    }
+                    Console.ReadKey();
+                }
+                else
+                {
+                    antalForkerte++;
+                    if (antalForkerte >= 3)
+                    {
+                        Pos.EmptyBox(13, 14);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Pos.Skriv(32, 15 + 1, $"Du ramte rigtigt {antalRigtige} gange og mangler derfor {3 - antalRigtige} rigtige");
+                        Pos.Skriv(32, 15 + 2, "svar for at vinde. Tak fordi du spillede med. Fortsat");
+                        Pos.Skriv(32, 15 + 3, "god festival!");
+                        Console.ResetColor();
+                        do
+                        {
+                            Pos.Skriv(32, 20, "Tryk på [ENTER] for at gå videre");
+                        }
+                        while (Console.ReadKey().Key != ConsoleKey.Enter);
+                        i = 6;
+                    }
+                    else
+                    {
+                        //Pos.EmptyBox(13, 14);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Pos.Skriv(32, 19,$"Det var desværre forkert. Det rigtige tal var: ");
+                        Console.ResetColor();
+                        Console.WriteLine(nummer);
+                        Pos.Skriv(32, 20,"Tryk på [ENTER] for at gå videre");
+                        Console.ReadKey();
+                    }
+                }
+            }
+        }
+    }
+}
